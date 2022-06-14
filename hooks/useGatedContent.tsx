7b97798c8ea519defde4f7b3d5ff94ac006e3af4
Ovie Okeh <ovie.okeh@and.digital>
2022-06-14 21:25:29 +0200

    add debugging for usegatedcontent

--- hooks/useGatedContent.tsx
+++ hooks/useGatedContent.tsx
@@ -7,15 +7,20 @@ export function useGatedContent(content: any) {
   let [render, setRender] = useState(<Construction />);
 
   useIsomorphicLayoutEffect(() => {
-    if (
+    console.log("RAN");
+
+    const shouldRender =
       (typeof window !== "undefined" && !(window as any).ENV.IS_DEV) ||
       [
         "http://localhost:3000/",
         "http://localhost:3000/companies",
         "https://worksponsor.nl/",
         "https://www.worksponsor.nl/",
-      ].some((url) => url === (window as any).ENV.URL)
-    ) {
+      ].some((url) => url === (window as any).ENV.URL);
+
+    console.log({ shouldRender, url: (window as any).ENV.URL });
+
+    if (shouldRender) {
       setRender(content);
     }
   }, []);
